6735fccad437ab292628db498ea746b0de1c75dc
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2018-08-24 22:14:55 +0400

    init app

--- second.xamarin/second.xamarin.Android/MainActivity.cs
+++ second.xamarin/second.xamarin.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace second.xamarin.Droid
 {
-    [Activity(Label = "second.xamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "second.xamarin", Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar.FullScreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- second.xamarin/second.xamarin/MainPage.xaml.cs
+++ second.xamarin/second.xamarin/MainPage.xaml.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using Xamarin.Forms;
 
 namespace second.xamarin
 {
     public partial class MainPage : ContentPage
     {
+        Timer timer = new Timer(10000);
+        Random random = new Random();
+
         public MainPage()
         {
             InitializeComponent();
+
+            timer.AutoReset = true;
+            timer.Elapsed += new ElapsedEventHandler(DispatcherTimer_Tick);
+            timer.Start();
+
+            ChangeBackgrdound();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            ChangeBackgrdound();
+        }
+
+        void ChangeBackgrdound()
+        {
+            stackLayout.BackgroundColor =
+                Color.FromRgb(
+                    (byte)random.Next(255),
+                    (byte)random.Next(255),
+                    (byte)random.Next(255)
+                    );
         }
     }
 }
